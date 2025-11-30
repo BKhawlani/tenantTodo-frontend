528a8d6195f258fbc2ab666bdec5f27893c50d10
Bkhawlani <basharalkhawlani@gmail.com>
2025-11-30 23:45:56 +0300

    Change API base URL to production environment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://tenanttodo-app.onrender.com/",
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
